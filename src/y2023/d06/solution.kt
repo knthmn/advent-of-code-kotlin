beab467fbd36b7d7255b790ad8ab530096924b0e
Kent Hou Man <knthmn0@gmail.com>
2023-12-06 19:30:49 +1100

    Improve 2023 day 6 solution

--- src/y2023/d06/solution.kt
+++ src/y2023/d06/solution.kt
@@ -1,6 +1,21 @@
 package y2023.d06
 
 import AdventOfCode
+import kotlin.math.ceil
+import kotlin.math.floor
+import kotlin.math.pow
+import kotlin.math.sqrt
+
+fun getNumWays(time: Long, distance: Long): Int {
+    val time = time.toDouble()
+    val distance = distance.toDouble()
+    val discriminant = time.pow(2) - 4 * distance
+    // this check is technically correct despite not required for the test case and submission
+    if (discriminant <= 0) return 0
+    val lower = (time - sqrt(discriminant)) / 2
+    val upper = (time + sqrt(discriminant)) / 2
+    return (if (upper % 1 == 0.0) upper - 1 else floor(upper)).toInt() - (if (lower % 1 == 0.0) lower + 1 else ceil(lower)).toInt() + 1
+}
 
 class y2023d06p1 : AdventOfCode(
     {
@@ -8,16 +23,14 @@ class y2023d06p1 : AdventOfCode(
     },
     { lines ->
         val parsedLines = lines.map { line ->
-            Regex("\\d+").findAll(line).map { it.value }
+            Regex("\\d+").findAll(line).map { it.value.toLong() }
         }
         val races = parsedLines[0].zip(parsedLines[1]).map {
-            it.first.toInt() to it.second.toInt()
+            it.first to it.second
         }.toList()
         races.fold(1) { acc, race ->
             val (time, distance) = race
-            acc * (1..time).count { chargeTime ->
-                chargeTime * (time - chargeTime) > distance
-            }
+            acc * getNumWays(time, distance)
         }
     },
 )
@@ -28,9 +41,6 @@ class y2023d06p2 : AdventOfCode(
     },
     { lines ->
         val (time, distance) = lines.map { it.replace("\\D".toRegex(), "") }.map { it.toLong() }
-        // It's not a lot of values to check anyway :D
-        (1..time).count { chargeTime ->
-            chargeTime * (time - chargeTime) > distance
-        }
+        getNumWays(time, distance)
     },
 )
